9f709e34bbe6f5069e522bd27f2fd31041f1864e
moojing <starts789@gmail.com>
2022-01-02 22:25:02 +0800

    feat: add claim button.

---
+++ scripts/3-config-nft.js
@@ -0,0 +1,21 @@
+import sdk from "./1-initialize-sdk.js";
+import { readFileSync } from "fs";
+
+const bundleDrop = sdk.getBundleDropModule(
+  process.env.APP_ADDRESS,
+);
+
+(async () => {
+  try {
+    await bundleDrop.createBatch([
+      {
+        name: "Web3kongFuDAO Membership NFT",
+        description: "This NFT will give you access to Web3kongFuDAO!",
+        image: readFileSync("scripts/assets/member.jpg"),
+      },
+    ]);
+    console.log("✅ Successfully created a new NFT in the drop!");
+  } catch (error) {
+    console.error("failed to create the new NFT", error);
+  }
+})()
